9722a271f87bc4810572e8a429ad6e85db3aed6b
Dean Gelbaum <deanscottg@gmail.com>
2020-07-07 18:26:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,8 +27,9 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
- element = array.[index_number] 
- p element
+ array.[index_number] 
+ 
+ 
 
   
 end
